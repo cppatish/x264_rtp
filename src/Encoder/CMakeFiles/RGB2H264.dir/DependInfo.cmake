93aa315035503cdef038824cc7845645f715919e
guanzhixiang <guanzhixiang@pset.suntec.net>
2017-06-15 16:25:39 +0800

    update
    Change-Id: I6395c3aa545881a6f67b563cf94ffa6cf9f83e48

---
+++ src/Encoder/CMakeFiles/RGB2H264.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/mass/workspace/rtp_connect/src/Encoder/rgbToH264.cpp" "/mass/workspace/rtp_connect/src/Encoder/CMakeFiles/RGB2H264.dir/rgbToH264.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
